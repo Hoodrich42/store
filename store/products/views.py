177ec74627da7d57d0831b4e13a1c752f1579616
Artem <asylguzhin-a@mail.ru>
2024-12-26 19:54:59 +0500

    create ProductView

--- store/products/views.py
+++ store/products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from products.models import Products
+
+
+class ProductView(DetailView):
+    model = Products
+    template_name = 'products/single_product_variable.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product'] = Products.objects.filter(pk=self.kwargs['pk'])
+        print(context['product'])
+
